101f816e6bc589acd1d9d415635e6318dc54a648
fren_gor <goro@frengor.com>
2022-12-06 04:45:38 +0100

    Reimplement collection auto-triggering

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,12 @@
 use std::cell::{BorrowMutError, RefCell};
 use std::ops::DerefMut;
 use std::thread::AccessError;
+
 use thiserror::Error;
 
 use crate::state::State;
+
+const DEFAULT_BYTES_THRESHOLD: usize = 100;
 
 thread_local! {
     pub(crate) static CONFIG: RefCell<Config> = RefCell::new(Config::default());
@@ -27,42 +30,21 @@ pub enum ConfigAccessError {
 #[derive(Debug, Clone)]
 pub struct Config {
     // The invariant is:
-    // adjustment_percent <= (allocated_bytes / bytes_threshold) <= trigger_percent
+    // bytes_threshold * adjustment_percent < allocated_bytes < bytes_threshold
     bytes_threshold: usize,
-    trigger_percent: f64,
     adjustment_percent: f64,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
-            bytes_threshold: 100,
-            trigger_percent: 0.7,
+            bytes_threshold: DEFAULT_BYTES_THRESHOLD,
             adjustment_percent: 0.25,
         }
     }
 }
 
 impl Config {
-    #[inline]
-    pub fn collection_trigger_percent(&self) -> f64 {
-        self.trigger_percent
-    }
-
-    #[inline]
-    #[track_caller]
-    pub fn set_collection_trigger_percent(&mut self, percent: f64) {
-        assert!(
-            percent > 0f64 && percent < 1f64,
-            "percent must be between 0 and 1 (excluded)"
-        );
-        assert!(
-            percent > self.adjustment_percent,
-            "percent must be greater than adjustment percent"
-        );
-        self.trigger_percent = percent;
-    }
-
     #[inline]
     pub fn adjustment_percent(&self) -> f64 {
         self.adjustment_percent
@@ -75,23 +57,42 @@ impl Config {
             percent > 0f64 && percent < 1f64,
             "percent must be between 0 and 1 (excluded)"
         );
-        assert!(
-            percent < self.trigger_percent,
-            "percent must be less than collection trigger percent"
-        );
         self.adjustment_percent = percent;
     }
 
     #[inline(always)]
     pub(crate) fn should_collect(&mut self, state: &State) -> bool {
-        state.allocated_bytes() as f64 > self.trigger_percent * self.bytes_threshold as f64
+        state.allocated_bytes() > self.bytes_threshold
     }
 
     #[inline(always)]
     pub(crate) fn adjust(&mut self, state: &State) {
-        if (state.allocated_bytes() as f64) < self.adjustment_percent * self.bytes_threshold as f64
-        {
-            self.bytes_threshold = (state.allocated_bytes() as f64 / self.trigger_percent) as usize;
+        if state.allocated_bytes() >= self.bytes_threshold {
+            let Some(new_threshold) = self.bytes_threshold.checked_mul(2) else { return; };
+            self.bytes_threshold = new_threshold;
+            while state.allocated_bytes() >= self.bytes_threshold {
+                let Some(new_threshold) = self.bytes_threshold.checked_mul(2) else { return; };
+                self.bytes_threshold = new_threshold;
+            }
+            return;
+        }
+
+        let allocated = state.allocated_bytes() as f64;
+        let mut bytes_threshold = self.bytes_threshold;
+        if allocated <= ((self.bytes_threshold as f64) * self.adjustment_percent) {
+            bytes_threshold <<= 1;
+            if bytes_threshold <= DEFAULT_BYTES_THRESHOLD {
+                self.bytes_threshold = DEFAULT_BYTES_THRESHOLD;
+                return;
+            }
+            while allocated <= ((self.bytes_threshold as f64) * self.adjustment_percent) {
+                bytes_threshold <<= 1;
+                if bytes_threshold <= DEFAULT_BYTES_THRESHOLD {
+                    self.bytes_threshold = DEFAULT_BYTES_THRESHOLD;
+                    return;
+                }
+            }
+            self.bytes_threshold = bytes_threshold;
         }
     }
 }
