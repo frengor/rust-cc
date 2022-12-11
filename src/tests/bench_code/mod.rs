274112f02b70fac2d0ce6b30e3be7b8e629508e3
fren_gor <goro@frengor.com>
2022-12-11 21:49:33 +0100

    Add tests for Cc::finalize_again and Cc::already_finalized

--- src/tests/bench_code/mod.rs
+++ src/tests/bench_code/mod.rs
@@ -35,13 +35,13 @@ fn test() {
             assert_eq!(first.len(), 0);
             if let Some((second, third)) = second.split_once('-') {
                 assert_eq!(second.len(), 5);
-                assert_eq!(third.len(), 0);
+                assert_eq!(third.len(), 1);
                 assert!(second.contains('A'));
                 assert!(second.contains('B'));
                 assert!(second.contains('C'));
                 assert!(second.contains('D'));
                 assert!(second.contains('E'));
-                //assert!(third.contains('D'));
+                assert!(third.contains('D'));
             } else {
                 panic!("String doesn't contains a second -.");
             }
@@ -96,7 +96,10 @@ fn two() {
     GLOBAL_CC.with(|global| {
         // Drop the Cc, if present
         // Note that this shouldn't require to call collect_cycles()
-        global.take()
+        if let Some(mut cc) = global.take() {
+            assert!(cc.already_finalized());
+            cc.finalize_again();
+        }
     });
 }
 
