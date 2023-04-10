10b051d97c5ce8b13075a5218579617a60f37caa
fren_gor <goro@frengor.com>
2023-04-10 15:34:54 +0200

    Use iai for benchmarks
    iai allows to run benchmarks in CI. Also, Criterion benchmarks are already provided by [rust-cc-benchmarks](https://github.com/frengor/rust-cc-benchmarks).

---
+++ benches/benches/binary_trees.rs
@@ -0,0 +1,70 @@
+//! Benchmark adapted from the shredder crate, released under MIT license. Src: https://github.com/Others/shredder/blob/266de5a3775567463ee82febc42eed1c9a8b6197/benches/shredder_benchmark.rs
+
+use std::hint::black_box;
+
+use rust_cc::*;
+
+// BENCHMARK 2: It's binary-trees from the benchmarks game!
+
+fn count_binary_trees(max_size: usize) -> Vec<usize> {
+    let mut res = Vec::new();
+    {
+        let min_size = 4;
+
+        for depth in (min_size..max_size).step_by(2) {
+            let iterations = 1 << (max_size - depth + min_size);
+            let mut check = 0;
+
+            for _ in 1..=iterations {
+                check += Cc::new(TreeNode::new(depth)).check();
+            }
+
+            res.push(check);
+        }
+    }
+    collect_cycles();
+    res
+}
+
+enum TreeNode {
+    Nested {
+        left: Cc<TreeNode>,
+        right: Cc<TreeNode>,
+    },
+    End,
+}
+
+unsafe impl Trace for TreeNode {
+    fn trace(&self, ctx: &mut Context<'_>) {
+        if let Self::Nested { left, right } = self {
+            left.trace(ctx);
+            right.trace(ctx);
+        }
+    }
+}
+
+impl Finalize for TreeNode {}
+
+impl TreeNode {
+    fn new(depth: usize) -> Self {
+        if depth == 0 {
+            return Self::End;
+        }
+
+        Self::Nested {
+            left: Cc::new(Self::new(depth - 1)),
+            right: Cc::new(Self::new(depth - 1)),
+        }
+    }
+
+    fn check(&self) -> usize {
+        match self {
+            Self::End => 1,
+            Self::Nested { left, right } => left.check() + right.check() + 1,
+        }
+    }
+}
+
+pub fn benchmark_count_binary_trees() {
+    count_binary_trees(black_box(11));
+}
---
+++ benches/benches/stress_test.rs
@@ -0,0 +1,63 @@
+//! Benchmark adapted from the shredder crate, released under MIT license. Src: https://github.com/Others/shredder/blob/266de5a3775567463ee82febc42eed1c9a8b6197/benches/shredder_benchmark.rs
+
+use std::cell::RefCell;
+use std::hint::black_box;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
+use rand::seq::SliceRandom;
+
+use rust_cc::*;
+
+// BENCHMARK 1: My janky stress test
+// (It basically creates a graph where every node is rooted, then de-roots some nodes a few at a time)
+struct DirectedGraphNode {
+    _label: String,
+    edges: Vec<Cc<RefCell<DirectedGraphNode>>>,
+}
+
+unsafe impl Trace for DirectedGraphNode {
+    fn trace(&self, ctx: &mut Context<'_>) {
+        self.edges.iter().for_each(|elem| elem.trace(ctx));
+    }
+}
+
+impl Finalize for DirectedGraphNode {}
+
+const NODE_COUNT: usize = 1 << 15;
+const EDGE_COUNT: usize = 1 << 15;
+const SHRINK_DIV: usize = 1 << 10;
+
+fn stress_test(rng: &mut StdRng) -> Vec<usize> {
+    let mut res = Vec::new();
+    {
+        let mut nodes = Vec::new();
+
+        for i in 0..=NODE_COUNT {
+            nodes.push(Cc::new(RefCell::new(DirectedGraphNode {
+                _label: format!("Node {}", i),
+                edges: Vec::new(),
+            })));
+        }
+
+        for _ in 0..=EDGE_COUNT {
+            let a = nodes.choose(rng).unwrap();
+            let b = nodes.choose(rng).unwrap();
+
+            a.borrow_mut().edges.push(Cc::clone(b));
+        }
+
+        for i in 0..NODE_COUNT {
+            if i % SHRINK_DIV == 0 {
+                nodes.truncate(NODE_COUNT - i);
+                collect_cycles();
+                res.push(nodes.len());
+            }
+        }
+    }
+    collect_cycles();
+    res
+}
+
+pub fn benchmark_stress_test() {
+    stress_test(black_box(&mut StdRng::seed_from_u64(0xCAFE)));
+}
