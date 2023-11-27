6fc226c19c9a5e3a3f631c20b7dd887ea188a69f
fren_gor <goro@frengor.com>
2023-11-27 23:15:52 +0100

    Add test for buffered objs threshold auto-collect

--- tests/auto_collect.rs
+++ tests/auto_collect.rs
@@ -90,3 +90,70 @@ fn test_disable_auto_collect() {
     }
     collect_cycles(); // Make sure to don't leak test's memory
 }
+
+#[test]
+fn test_buffered_threshold_auto_collect() {
+    const MAX_BUFFERED_OBJS: usize = 4;
+
+    // Always reset buffered objs threshold and adjustment percent, even with panics
+    struct DropGuard(f64, usize);
+    impl Drop for DropGuard {
+        fn drop(&mut self) {
+            config(|config| {
+                config.set_adjustment_percent(self.0);
+                config.set_buffered_objects_threshold(self.1);
+            }).expect("Couldn't reset buffered objs threshold and adjustment percent");
+        }
+    }
+    let _drop_guard = config(|config| {
+        let guard = DropGuard(config.adjustment_percent(), config.buffered_objects_threshold());
+        config.set_adjustment_percent(0.0);
+        config.set_buffered_objects_threshold(3);
+        guard
+    }).expect("Couldn't set buffered objs threshold and adjustment percent");
+
+    struct Cyclic<T: 'static> {
+        cyclic: RefCell<Option<Cc<Cyclic<T>>>>,
+        _t: T,
+    }
+
+    unsafe impl<T: 'static> Trace for Cyclic<T> {
+        fn trace(&self, ctx: &mut Context<'_>) {
+            self.cyclic.trace(ctx);
+        }
+    }
+
+    impl<T: 'static> Finalize for Cyclic<T> {
+    }
+
+    fn new<T: Default + 'static>() -> Cc<Cyclic<T>> {
+        let cc = Cc::new(Cyclic {
+            cyclic: RefCell::new(None),
+            _t: Default::default(),
+        });
+        *cc.cyclic.borrow_mut() = Some(cc.clone());
+        cc
+    }
+
+    // Increase bytes_threshold
+    {
+        let _big = new::<Big>();
+        collect_cycles();
+    }
+    collect_cycles();
+
+    let executions_counter = executions_count().unwrap();
+
+    assert_eq!(0, rust_cc::state::buffered_objects_count().unwrap());
+
+    for _ in 0..MAX_BUFFERED_OBJS {
+        let _ = new::<()>();
+
+        assert_eq!(executions_counter, executions_count().unwrap(), "Collected but shouldn't have collected.");
+    }
+
+    let _ = new::<()>();
+
+    assert_eq!(executions_counter + 1, executions_count().unwrap(), "Didn't collected");
+    collect_cycles(); // Make sure to don't leak test's memory
+}
